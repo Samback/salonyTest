ace35dfccd6f2209d8c2a510ddff745d107fd1a5
Max Tymchii <samback@ukr.net>
2017-07-24 14:41:29 +0300

    Add base appearance. Color and fonts extensions,

---
+++ salonyTest/salonyTest/AppearanceController.swift
@@ -0,0 +1,45 @@
+//
+//  AppearanceController.swift
+//  salonyTest
+//
+//  Created by Max Tymchii on 7/24/17.
+//  Copyright © 2017 SambackInc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Rswift
+
+struct AppearanceController {
+    func apply() {
+        
+    }
+    
+    
+    private func applyAppearanceForNavigationBar() {
+        self.configurebackButton()
+        self.configureTitle()
+    }
+    
+    private func configureTitle() {
+        let navigationBar = UINavigationBar.appearance()
+        navigationBar.barTintColor = Color.rgb1583098
+        navigationBar.tintColor = .white
+        
+        let font = Font.regularFont(size: .p18)
+        navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.white,
+                                             NSFontAttributeName: font]
+        
+        let img = UIImage()
+        navigationBar.shadowImage = img
+    }
+    
+    private func configurebackButton() {
+        let navigationBar = UINavigationBar.appearance()
+        
+        let image = R.image.back()
+        navigationBar.backIndicatorTransitionMaskImage = image
+        navigationBar.backIndicatorImage = image
+    }
+
+}
